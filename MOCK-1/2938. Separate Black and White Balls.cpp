3e306fb6109ab38f06fca1e9233665b630e12c26
Harish Kumar V <166478902+Harish200510@users.noreply.github.com>
2025-05-18 22:52:39 +0530

    Update 2938. Separate Black and White Balls.cpp

--- MOCK-1/2938. Separate Black and White Balls.cpp
+++ MOCK-1/2938. Separate Black and White Balls.cpp
@@ -1,8 +1,17 @@
+key idea:
+    -> Find the zeros fron the last and store the indicies in array (arr)
+    ->check if current s[i]=="1" and i<arr[j] then add the steps to swap with that zero(abs(i-arr[j])) make j<arr.size()
+
+
 class Solution {
 public:
     long long minimumSteps(string s) {
+       
         long long moves=0;
+        //To store zero indices location
         vector<int>arr;
+      
+        //Iterating from last and finding zeros and storing there indicies
         for(int i=s.size()-1;i>=0;i--)
         {
             if(s[i]=='0')
@@ -10,7 +19,9 @@ public:
             arr.push_back(i);
             }
         }
+        
         int j=0;
+        
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='1' && !arr.empty() && j<arr.size())
@@ -22,6 +33,7 @@ public:
                 }
             }
         }
+       
         return moves;
         
     }
