9f020c4282e135bfb3a67c362ad60d6a483a6207
Harish Kumar V <166478902+Harish200510@users.noreply.github.com>
2025-05-19 11:05:27 +0530

    Update 2938. Separate Black and White Balls.cpp

--- MOCK-1/2938. Separate Black and White Balls.cpp
+++ MOCK-1/2938. Separate Black and White Balls.cpp
@@ -3,8 +3,7 @@ key idea:
     ->check if current s[i]=="1" and i<arr[j] then add the steps to swap with that zero(abs(i-arr[j])) make j<arr.size()
 
 
-class Solution {
-public:
+Better solution: TC->O(2N) and SC-> O(N)
     long long minimumSteps(string s) {
        
         long long moves=0;
@@ -37,4 +36,37 @@ public:
         return moves;
         
     }
-};
+
+Key idea:
+      -> Find the last zero that before one indicies and move that indicies one place ahead because we are going to place that upcoming zero in that place or we can say that we need to stand the first one 
+      ->Thats why we are starting from lastzerobeforeone=0
+       -> after finding the last  indicies  of continuous zero start from there
+       -> If you find any zero just add minmove+=abs(j-lastzerobeforeone); and move lastzerobeforeone++ because there only we are going to place the next zero 
+       
+
+optimal solution:TC-> O(N) and SC->O(1)
+
+long long minimumSteps(string s) {
+      
+       int n=s.size();
+      
+       long long minmoves=0;
+      
+       int lastzerobeforeone=0;
+       
+       int i=0;
+      
+       for(i=0;i<n;i++){
+          if(s[i]=='1')break;
+          lastzerobeforeone++;
+       }
+
+       for(int j=i;j<n;j++){
+              if(s[j]=='0'){
+                   minmoves+=abs(j-lastzerobeforeone);
+                   lastzerobeforeone++;
+              }
+       }
+
+       return minmoves;
+    }
